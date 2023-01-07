3adb6cab6c965cdbd2c04e17c1f213456361c312
yonmo <yonmo@idolum.io>
2023-01-07 18:44:08 -0500

    Added some comparison logic. Refactored the python boto3 client code

--- s3nse.py
+++ s3nse.py
@@ -2,64 +2,131 @@
 
 import os, boto3, dotenv, sys
 
-dotenv.load_dotenv()
+from botocore.exceptions import ClientError
 
 # !!!! OUT OF SCOPE !!!! (I SWEAR I WILL EAT YOUR LEGS) !!!! OUT OF SCOPE !!!! #
-REGION = os.getenv('REGION')
-ENDPOINT = os.getenv('ENDPOINT')
-ACCESS_ID = os.getenv('ACCESS_ID')
-SECRET_KEY = os.getenv('SECRET_KEY')
-BUCKET = os.getenv('BUCKET')
-# !!!! OUT OF SCOPE !!!! (I SWEAR I WILL EAT YOUR LEGS) !!!! OUT OF SCOPE !!!! #
 
-client = boto3.session.Session().client('s3', 
-                                    region_name=REGION, 
-                                    endpoint_url=ENDPOINT, 
-                                    aws_access_key_id=ACCESS_ID, 
-                                    aws_secret_access_key=SECRET_KEY)
+class s3nse:
+    def __init__(self):
+        dotenv.load_dotenv()
+        environment_keys = ['REGION', 'ENDPOINT', 'ACCESS_ID', 'SECRET_KEY', 'BUCKET']
+        self.configuration = tuple(os.getenv(x) for x in environment_keys)
+        self.session = boto3.session.Session()
+        self.configure()
+        pass
+
+    def configure(self):
+        try:
+            
+            self.session = self.session.client('s3', 
+                region_name = self.configuration[0],
+                endpoint_url = self.configuration[1],
+                aws_access_key_id = self.configuration[2],
+                aws_secret_access_key = self.configuration[3])
+
+        except IndexError as err:
+            print(err)
+            self.terminate()
+        pass
+
+    def terminate(self):
+        print('\nExiting s3nse...')
+        exit()
+
+# !!!! OUT OF SCOPE !!!! (I SWEAR I WILL EAT YOUR LEGS) !!!! OUT OF SCOPE !!!! #
 
 def listFiles(kargs):
     '''
         listFiles function for returning file names within an s3 bucket file based on BUCKET variable.
     '''
-    if kargs != None:
-        BUCKET = kargs[0]
+
     try:
+
+        if kargs != None:
+            BUCKET = kargs[0]
+
+        client = s3nse().session
         bucket = client.list_objects(Bucket=BUCKET)
         for file in bucket['Contents']:
             print(file['Key'])
-        return(0)
-    except:
-        return(1)
+
+    except ClientError as err:
+        print(err)
+        raise(ClientError)
+
+    except IndexError as err:
+        print(err)
+        raise(IndexError)
     
 def pullFiles(kargs):
     '''
         getFiles function for downloading files based on BUCKET and KEY variables.
     '''
-    if kargs != None:
-        BUCKET = kargs[0]
-        KEY = kargs[1]
+   
     try:
+
+        if kargs != None:
+            BUCKET = kargs[0]
+            KEY = kargs[1]
+
+        client = s3nse().session
         client.download_file(Bucket=BUCKET, Key=KEY, Filename=KEY)
-        return(0)
-    except:
-        return(1)
+
+    except ClientError as err:
+        print(err)
+        raise(ClientError)
+
+    except IndexError as err:
+        print(err)
+        raise(IndexError)
 
 def pushFiles(kargs):
     '''
         pushFiles function for uploading files based on BUCKET and KEY variables.
     '''
-    if kargs != None:
-        BUCKET = kargs[0]
-        KEY = kargs[1]
+
     try:
-        client.download_file(Bucket=BUCKET, Key=KEY, Filename=KEY)
-        return(0)
-    except:
-        return(1)
+
+        if kargs != None:
+            print(kargs)
+            BUCKET = kargs[0]
+            FILENAME = kargs[1]
+
+        client = s3nse().session
+        client.upload_file(Filename=FILENAME, Bucket=BUCKET, Key=FILENAME)
+
+    except ClientError as err:
+        print(err)
+        raise(ClientError)
+
+    except IndexError as err:
+        print(err)
+        raise(IndexError)
+
+def delFiles(kargs):
+    '''
+        delFiles function for deleting files based on BUCKET and KEY variables
+    '''
+
+    try:
+
+        if kargs != None:
+            BUCKET = kargs[0]
+            KEY = kargs[1]
+
+        client = s3nse().session
+        client.delete_object(Bucket=BUCKET, Key=KEY)
+        
+    except ClientError as err:
+        print(err)
+        raise(ClientError)
+
+    except IndexError as err:
+        print(err)
+        raise(IndexError)
 
 if __name__ == '__main__':
-    functions = [listFiles, pullFiles, pushFiles]
+    functions = [listFiles, pullFiles, pushFiles, delFiles]
     commands = dict({x.__qualname__: x for x in functions})
     try:
         args = sys.argv[1::]
@@ -67,6 +134,14 @@ if __name__ == '__main__':
             if (commands.get(args[0])):
                     output = commands.get(args[0])(args[1::] if len(args) > 1 else None)
                     print(output if output != None else '')
+    
+    except IndexError:
+        print('\nIncorrect number of arguments specified.')
+        s3nse.terminate()
+    
+    except ClientError:
+        print('\nClient error in the s3nse module.')    
+        s3nse.terminate()
+    
     except KeyboardInterrupt:
-        print('\nExiting s3nse...')
-        exit()
+        s3nse.terminate()
